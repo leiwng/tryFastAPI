26ff5ef294f060aea9c8e21ad3f59392e4ecb1f7
Lei Wang <leiwang@orientsoft.cn>
2020-10-26 17:48:32 +0800

    data model definition for cqupt epay project

---
+++ data_model.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+  data_model
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  Data Model for CQUPT EPAY project.
+
+  :copyright: (c) 2020/10/26 by wanglei.
+  :license: MIT
+
+TODO:
+"""
+
+# 大学
+class University():
+  pass
+
+# 院/系/所
+class School():
+  pass
+
+# 班级
+class Class():
+  pass
+
+# 用户
+class User():
+  pass
+
+# 竞赛模板
+class GameTemplate():
+  pass
+
+# 分组模板
+class TeamTemplate():
+  pass
+
+# 单轮竞赛
+class SingleRoundGame():
+  pass
+
+# 多轮竞赛
+class MultiRoundGame():
+  pass
+
+# 自由搏击
+class FreeBattleGame():
+  pass
+
+# 竞赛评估
+class GameAssessment():
+  pass
+
+class StudentAssessment():
+  pass
+
+class TeamAssessment():
+  pass
+
